f18bbd709e6ac4509f7f4aaa5264cc9327180285
Joel Martin <github@martintribe.org>
2013-01-24 21:45:33 -0600

    First pass at porting closure.clj and cljsc.clj.
    Build like this:
        ../bin/cljsc ../src/cljs/cljsc.cljs > cljsc.js && cp ../src/cljs/goog.js out
    
    Run like this:
        ./run.js cljsc.js hello.cljs > hello.js
    
    It will attempt to compile using out2 as the output directory
    and will blow up trying to compile core.cljs.

---
+++ src/cljs/cljsc.cljs
@@ -0,0 +1,30 @@
+;   Copyright (c) Rich Hickey. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
+(ns cljsc
+  (:require [cljs.closure :as closure]))
+
+(defn transform-cl-args
+  [args]
+  (let [source (first args)
+        opts-string (apply str (interpose " " (rest args)))
+        options (when (> (count opts-string) 1)
+                  (try (read-string opts-string)
+                       (catch Exception e (println e))))]
+    {:source source :options (merge {:output-to :print} options)}))
+
+(defn- -main
+  [& cmdline-args]
+
+  ;; Setup the print function
+  (set! *print-fn* (.-print (js/require "util")))
+
+  (let [args (transform-cl-args cmdline-args)]
+    (closure/build (:source args) (:options args))))
+
+(set! *main-cli-fn* -main)
